0ad13c3168d72e8b1fef142823364ae50964d26d
storhem <storhetmax@mail.ru>
2025-12-12 22:25:57 +0300

    added a color change button

--- color.py
+++ color.py
@@ -9,4 +9,8 @@ colors = ["red", "green", "blue", "yellow", "purple", "orange"]
 panel = tk.Label(root, bg="lightblue", width=40, height=15)
 panel.pack(pady=20, padx=20)
 
+tk.Button(root, text="Сменить цвет",
+          command=lambda: panel.config(bg=random.choice(colors)),
+          width=20).pack(pady=5)
+
 root.mainloop()
