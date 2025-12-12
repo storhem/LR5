5b6101153501caca2bc7d970b910576368af9719
storhem <storhetmax@mail.ru>
2025-12-12 22:29:17 +0300

    a submenu has been created

--- menu.py
+++ menu.py
@@ -5,6 +5,7 @@ root.title("Окно с меню")
 
 menu = tk.Menu(root)
 file_menu = tk.Menu(menu, tearoff=0)
+file_menu.add_command(label="Exit", command=root.quit)
 menu.add_cascade(label="File", menu=file_menu)
 root.config(menu=menu)
 
